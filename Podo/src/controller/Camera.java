e77a645675096d4330d6ef19e6badf5fa6eb33de
지성은 <jsunge0907@gmail.com>
2020-05-11 02:31:38 +0900

    ioujhiuojiuo

--- Podo/src/controller/Camera.java
+++ Podo/src/controller/Camera.java
@@ -27,7 +27,8 @@ public class Camera extends HttpServlet {
 	int cnt = 0;
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		/*
 		 * System.out.println("hi"); //이미지를 저장할 경로 입력. String folderTypePath =
@@ -46,67 +47,63 @@ public class Camera extends HttpServlet {
 		 * System.out.println("이미지를 저장하였습니다. : " + fileName); } catch (IOException e) {
 		 * System.out.println("안드로이드 부터 이미지를 받아옵니다."); }
 		 */
-		
-		    response.setCharacterEncoding("EUC-KR");
-	        PrintWriter out = response.getWriter();
-	         
-	        String id = request.getParameter("id");
-	        System.out.println(id+"잘넘어왔네");
-	         
-	         
-	         
-	      /////////////////////////////////////////////////////////////////////////
-	         
-	         String folderTypePath = "D:\\"+id;
-	         
-	         File Folder = new File(folderTypePath);
-	         
 
+		response.setCharacterEncoding("EUC-KR");
+		PrintWriter out = response.getWriter();
 
-	         // 해당 디렉토리가 없을경우 디렉토리를 생성합니다.
-	         if (!Folder.exists()) {
-	            try{
-	                Folder.mkdir(); //폴더 생성합니다.
-	                System.out.println("폴더가 생성되었습니다.");
-	                 } 
-	                 catch(Exception e){
-	                e.getStackTrace();
-	            }        
-	               }else {
-	            System.out.println("이미 폴더가 생성되어 있습니다.");
-	         }
-	      
-	   
-	        String name = new String();
-	        String fileName = new String();
-	        int sizeLimit = 10 * 1024 * 1024; // 5메가까지 제한 넘어서면 예외발생
-	 
-	        try {
-	           MultipartRequest multi = new MultipartRequest(request, folderTypePath, sizeLimit,
-	                    new DefaultFileRenamePolicy());
-	           
-	           
-	            Enumeration files = multi.getFileNames();
-	 
-	            //파일 정보가 있다면
-	            if (files.hasMoreElements()) {
-	               
-	                name = (String) files.nextElement();
-	                fileName = multi.getFilesystemName(name);
-	                dto = new BookDTO("용감한포도잼", cnt, "");
-	                dao.bookinsert(dto);
-	                cnt +=1;
-	               
-	            }
-	            System.out.println("이미지를 저장하였습니다. : " + fileName);
-	            
-	        } catch (IOException e) {
-	            System.out.println("안드로이드 부터 이미지를 받아옵니다.");
-	        }
-	        
-	        GoogleVisionApiTester GVA = new GoogleVisionApiTester();
-	        GVA.textreturn(folderTypePath + "\\", cnt);
+		String id = request.getParameter("id");
+		System.out.println(id + "잘넘어왔네");
 
+		/////////////////////////////////////////////////////////////////////////
+		// String folderTypePath = "D:\\voice";
+		String folderTypePath = "C:\\Users\\SM025\\git\\BGJ_Web\\Podo\\WebContent\\" + id;
+	//	String folderTypePath2 = "C:\\Users\\SM016\\git\\BGJ_Web\\Podo\\WebContent\\" + id + "\\voice";
+		File Folder = new File(folderTypePath);
+	//	File Folder2 = new File(folderTypePath2);
+
+		// 해당 디렉토리가 없을경우 디렉토리를 생성합니다.
+		if (!Folder.exists()) {
+			try {
+				Folder.mkdir(); // 폴더 생성합니다.
+				System.out.println("폴더가 생성되었습니다.");
+			} catch (Exception e) {
+				e.getStackTrace();
+			}
+		} else {
+			System.out.println("이미 폴더가 생성되어 있습니다.");
+		}
+
+		/*
+		 * if (!Folder2.exists()) { try { Folder.mkdir(); // 폴더 생성합니다.
+		 * System.out.println("폴더가 생성되었습니다."); } catch (Exception e) {
+		 * e.getStackTrace(); } } else { System.out.println("이미 폴더가 생성되어 있습니다."); }
+		 */
+		String name = new String();
+		String fileName = new String();
+		int sizeLimit = 10 * 1024 * 1024; // 5메가까지 제한 넘어서면 예외발생
+
+		try {
+			MultipartRequest multi = new MultipartRequest(request, folderTypePath, sizeLimit,
+					new DefaultFileRenamePolicy());
+
+			Enumeration files = multi.getFileNames();
+
+			// 파일 정보가 있다면
+			if (files.hasMoreElements()) {
+
+				name = (String) files.nextElement();
+				fileName = multi.getFilesystemName(name);
+				File file = new File(folderTypePath + "\\" + fileName);
+				File file2 = new File(folderTypePath + "\\voice.wav");
+				file2.delete();
+				file.renameTo(new File(folderTypePath + "\\voice.wav"));
+
+			}
+			System.out.println("이미지를 저장하였습니다. : " + fileName);
+
+		} catch (IOException e) {
+			System.out.println("안드로이드 부터 이미지를 받아옵니다.");
+		}
 
 	}
 
